e80c086f83338e41664b63e212fb9258e9db5026
masaru gotou <masaru.gotou@gmail.com>
2022-08-05 20:51:46 +0900

    リファクタリング。sample05追加。

--- find_tables_from_select.py
+++ find_tables_from_select.py
@@ -3,11 +3,15 @@
 """
 import sqlparse
 
+IS_SHOW_TOKEN = False
 
-def show_statement(tokens):
+
+def show_tokens(tokens):
     """
-    Statementを表示する
+    tokenを表示する
     """
+    if not IS_SHOW_TOKEN:
+        return
     for token in tokens:
         if token.is_whitespace:
             continue
@@ -27,22 +31,40 @@ def find_tables_from_select(tokens):
             if isinstance(token, sqlparse.sql.Identifier):
                 tables.append(token)
             if isinstance(tokens, sqlparse.sql.IdentifierList):
-                for id in token.get_identifiers():
+                for i in token.get_identifiers():
                     if token.match(sqlparse.tokens.Punctuation, "*", regex=True):
                         continue
-                    tables.append(id)
+                    tables.append(i)
         if isinstance(token, sqlparse.sql.Where):
-            print("<<where>>")
-            show_statement(token.tokens)
-            for wt in token.tokens:
-                if isinstance(wt, sqlparse.sql.Comparison):
-                    print("<<Comparison>>")
-                    show_statement(wt.tokens)
-                    for ct in wt.tokens:
-                        if isinstance(ct, sqlparse.sql.Parenthesis):
-                            print("<<Parenthesis>>")
-                            show_statement(ct.tokens)
-                            tables.extend(find_tables_from_select(ct.tokens))
+            tables.extend(find_tables_from_where(token.tokens))
+    return tables
+
+
+def find_tables_from_where(tokens):
+    """
+    WHERE句からテーブルを抽出する
+    """
+    tables = list()
+    print("<<where>>")
+    show_tokens(tokens)
+    for token in tokens:
+        if isinstance(token, sqlparse.sql.Comparison):
+            tables.extend(find_tables_from_parenthesis(token.tokens))
+    return tables
+
+
+def find_tables_from_parenthesis(tokens):
+    """
+    カッコ内のSELECT文からテーブルを抽出する
+    """
+    tables = list()
+    print("<<Comparison>>")
+    tokens(tokens)
+    for token in tokens:
+        if isinstance(token, sqlparse.sql.Parenthesis):
+            print("<<Parenthesis>>")
+            tokens(token.tokens)
+            tables.extend(find_tables_from_select(token.tokens))
     return tables
 
 
@@ -50,31 +72,25 @@ def main():
     """
     main
     """
-    with open('sample04.sql') as f:
+    with open('sample05.sql') as f:
         query = f.read()
     parsed_queries = sqlparse.parse(query)
-    
+
     statement = parsed_queries[0]
     tokens = statement.tokens
-    
+
     # Show Statement Tokens
     print("<<Statement>>")
-    show_statement(tokens)
-    
-    # print("<<Statement>>")
-    # for t in tokens:
-    #     if t.is_whitespace:
-    #         continue
-    #     print("%s %s ttype=%s" % (t.value,type(t),t.ttype))
-    
+    show_tokens(tokens)
+
     # Find Tables
     tables = list()
 
     tables.extend(find_tables_from_select(tokens))
-    
+
     print("<<detected tables>>")
     for tbl in tables:
-        print("%s(alias=%s) %s(ttype=%s)" % (tbl.get_real_name(),tbl.get_alias(),type(tbl),tbl.ttype))
+        print("%s(alias=%s) %s(ttype=%s)" % (tbl.get_real_name(), tbl.get_alias(), type(tbl), tbl.ttype))
 
 
 if __name__ == "__main__":
